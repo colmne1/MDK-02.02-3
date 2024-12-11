57e76eabe0c00330b4f72aa6d4adc1b10dc8295a
yurak <yurak@DESKTOP-DMDF3TH>
2024-12-11 18:27:07 +0500

    Созданный метод загрузки данных о результатах игроков. Kochetov

--- Snake_Kochetov/Program.cs
+++ Snake_Kochetov/Program.cs
@@ -220,5 +220,17 @@ namespace Snake_Kochetov
             SW.WriteLine(json);
             SW.Close();
         }
+        public static void LoadLeaders()
+        {
+            if (File.Exists("./leaders.txt"))
+            {
+                StreamReader SR = new StreamReader("./leaders.txt");
+                string json = SR.ReadLine();
+                SR.Close();
+                if (!string.IsNullOrEmpty(json)) Leaders = JsonConvert.DeserializeObject<List<Leaders>>(json);
+                else Leaders = new List<Leaders>();
+            }
+            else Leaders = new List<Leaders>();
+        }
     }
 }
