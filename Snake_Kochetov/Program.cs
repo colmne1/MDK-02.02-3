5cde3a30c85fe19b11dc1c09014fd27b90c20f6c
yurak <yurak@DESKTOP-DMDF3TH>
2024-12-11 18:38:39 +0500

    Установил Json

--- Snake_Kochetov/Program.cs
+++ Snake_Kochetov/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
+﻿using Common;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
-using System.Threading.Tasks;
-using System.Data.Common;
-using System.Net.Sockets;
-using System.Net;
-using Common;
 using System.Threading;
-using System.IO;
+using System.Threading.Tasks;
 
 namespace Snake_Kochetov
 {
