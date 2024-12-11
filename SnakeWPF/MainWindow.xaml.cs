49d19ad8d34211959c7d2952ec9c470fe67cea38
Shizyn412 <juetjuifjhhsiehykeh@mail.ru>
2024-12-11 22:31:46 +0500

    Метод отправки данных на сервер. Осипов

--- SnakeWPF/MainWindow.xaml.cs
+++ SnakeWPF/MainWindow.xaml.cs
@@ -90,5 +90,25 @@ namespace SnakeWPF
                 Debug.WriteLine("Возникло исключение: " + ex.ToString() + "\n" + ex.Message);
             }
         }
+        public static void Send(string datagram)
+        {
+            UdpClient client = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(remoteIPAddress, remotePort);
+
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(datagram);
+
+                client.Send(bytes, bytes.Length, endPoint);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Возникло исключение: " + ex.ToString() + "\n" + ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
     }
 }
