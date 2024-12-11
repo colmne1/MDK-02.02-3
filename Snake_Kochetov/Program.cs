3589356f5fd3495ee9631e5da6d502fdeb3b7db4
yurak <yurak@DESKTOP-DMDF3TH>
2024-12-11 17:06:02 +0500

    Добавленные переменные сервера. Kochetov

--- Snake_Kochetov/Program.cs
+++ Snake_Kochetov/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,10 @@ namespace Snake_Kochetov
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-        }
+        public static List<Leaders> Leaders = new List<Leaders>();
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+        private static int localPort = 5001;
+        public static int MaxSpeed = 15;
     }
 }
