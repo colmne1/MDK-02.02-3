b0c96d08b50a3a2bc6e5feea74bf82bddf613a04
yurak <yurak@DESKTOP-DMDF3TH>
2024-12-11 17:33:24 +0500

    Созданный метод Send. Кочетов

--- Snake_Kochetov/Program.cs
+++ Snake_Kochetov/Program.cs
@@ -5,6 +5,8 @@ using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Common;
+using System.Net.Sockets;
+using System.Net;
 
 namespace Snake_Kochetov
 {
@@ -15,5 +17,35 @@ namespace Snake_Kochetov
         public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
         private static int localPort = 5001;
         public static int MaxSpeed = 15;
-    }
+        private static void Send()
+        {
+            foreach (var User in remoteIPAddress)
+            {
+                UdpClient sender = new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(
+                    IPAddress.Parse(User.IPAddress),
+                    int.Parse(User.Port));
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
+
+                    sender.Send(bytes, bytes.Length, endPoint);
+
+                    bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelGames.FindAll(x => x.IdSnake != User.IdSnake)));
+                    sender.Send(bytes, bytes.Length, endPoint);
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Отправил данные пользователю {User.IPAddress}:{User.Port}");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n " + ex.Message);
+                }
+                finally
+                {
+                    sender.Close();
+                }
+            }
+        }
 }
